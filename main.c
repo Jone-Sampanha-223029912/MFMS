82414cd68e2052223d84ceb26065e7be25b91dcb
Jone Sampanha <223029912@nust.na>
2026-09-25 17:31:43 +0200

    Initial MFMS project setup

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("========================================\n");
+    printf(" MUNICIPAL FINANCIAL MANAGEMENT SYSTEM\n");
+    printf("========================================\n");
+    printf("1. Employee Management\n");
+    printf("2. Budget Management\n");
+    printf("3. Supplier Management\n");
+    printf("4. Asset Management\n");
+    printf("5. Reports\n");
+    printf("6. Exit\n");
+    printf("Select an option from the menu 1-6: ");
+
+    return 0;
+}
